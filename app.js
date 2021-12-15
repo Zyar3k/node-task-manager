b75d385cde68911f6a7f73f5c6aaabc0132baf68
zyar3k <a.zygan@gmail.com>
2021-12-15 17:19:47 +0100

    Add routes pattern

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("task start");
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const PORT = 3000;
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
