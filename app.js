dfdbf24ef1cdba02f749b91a6ffb421b941124dd
zyar3k <a.zygan@gmail.com>
2021-12-16 22:08:43 +0100

    Improve controllers & finish project

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
 app.use(express.static("./public"));
@@ -11,6 +13,8 @@ app.use(express.json());
 
 // routes
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
