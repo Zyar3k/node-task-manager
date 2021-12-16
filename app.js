ba6bc4c711392a2263d4eba89e120db18311a27a
zyar3k <a.zygan@gmail.com>
2021-12-16 19:52:04 +0100

    Connect to db

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const PORT = 3000;
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
@@ -13,6 +15,12 @@ app.get("/hello", (req, res) => {
 
 app.use("/api/v1/tasks", tasks);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+start();
